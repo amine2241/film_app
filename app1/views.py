741cdf97971a6c77941ff9721a48545eb69c3bb5
amine2241 <aminelakbachi21@gmail.com>
2023-05-24 01:29:36 +0100

    navebar

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'app1/home.html')
 
+
+def about(request):
+    return render(request, 'app1/about.html')
+
 # Create your views here.
